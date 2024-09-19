16477b38f4d25059e38bda7accdf5582c75bd70f
emimedina32 <emilianomedina357@gmail.com>
2024-09-19 09:36:45 -0300

    hice las tablas de realData

---
+++ tablas_RM/competencias.sql
@@ -0,0 +1,8 @@
+CREATE TABLE Competencias (
+    id_Competencia INT AUTO_INCREMENT PRIMARY KEY,
+    nombre_Competencia TEXT,
+    Tipo VARCHAR(20),
+    Resultado_Final VARCHAR(50),
+    id_Titulo INT,
+    FOREIGN KEY (id_Titulo) REFERENCES Titulos(id_Titulo)
+);
---
+++ tablas_RM/titulos.sql
@@ -0,0 +1,7 @@
+CREATE TABLE Titulos (
+    id_Titulo INT AUTO_INCREMENT PRIMARY KEY,
+    Nombre_Titulo VARCHAR(30),
+    Competencia VARCHAR(30),
+    Fecha_Ganado DATE,
+    Numero_Titulo INT
+);
