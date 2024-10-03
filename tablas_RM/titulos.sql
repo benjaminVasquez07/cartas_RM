a9834414a2dba5936cf4773a2023d974b40ec1cd
agustindiaz <diazagustiin30@gmail.com>
2024-10-03 11:56:15 -0300

    me falta corregir el insert de la tabla titulos

--- tablas_RM/titulos.sql
+++ tablas_RM/titulos.sql
@@ -5,3 +5,10 @@ CREATE TABLE Titulos (
     Fecha_Ganado DATE,
     Numero_Titulo INT
 );
+--inser de titulos 
+INSERT INTO TITULOS (Nombre_Titulo, Competencia, Fecha_Ganado, Número_Titulo)
+VALUES ('LaLiga', 'Liga Española', '2023-05-20', 35
+        'UEFA Champions League', 'Competición Europea', '2022-05-28', 14
+        'Copa del Rey', 'Competición Nacional', '2023-04-06', 20
+        'Supercopa de España', 'Competición Nacional', '2022-01-16', 12);
+        
